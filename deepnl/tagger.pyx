d14b20d3d2d3d8a63fcff53bdf78c3555575e5e3
kun09-tker <ltanloc97o@gmail.com>
2024-09-15 13:48:07 +0700

    a

--- deepnl/tagger.pyx
+++ deepnl/tagger.pyx
@@ -14,6 +14,7 @@ import sys                      # DEBUG
 
 # local
 from deepnl.network cimport *
+from deepnl.network import *
 from deepnl.networkseq import SequenceNetwork
 from numpy import int32 as INT
 
